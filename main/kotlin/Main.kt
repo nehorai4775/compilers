e48fc8cbd8579a4293aa5adb3f6559d5ae36d5e0
didi <ymarashe@g.jct.ac.il>
2022-03-14 12:38:45 +0200

    start tar 1

--- main/kotlin/Main.kt
+++ main/kotlin/Main.kt
@@ -1,12 +1,14 @@
 //Yedidya Marashe 213661499
 //Nehorai Cohen 325356814
-//150060.01.5782.01, 150060.01.5782.48    
-
-
+//150060.01.5782.01, 150060.01.5782.48    \
+package VMcompiler
 import java.io.File
 import java.io.InputStream
-
 var asmFile: File? = null
+String pathSeparator = File.pathSeparator;
+val os = System.getProperty("os.name")
+
+
 
 fun main(args: Array<String>) {
 
@@ -14,6 +16,7 @@ fun main(args: Array<String>) {
     val vmFiles = setUpFiles(args[0]);
     iterateFiles(vmFiles);
 }
+
 fun setUpFiles(dirName : String) : List<File>
 {
     //iterate through the directory and find the two .vm files
@@ -26,11 +29,9 @@ fun setUpFiles(dirName : String) : List<File>
     asmFile?.createNewFile();
     return vmFiles;
 }
+
 fun iterateFiles(vmFiles: List<File>)
 {
-
-    var totalSales : Double = 0.0;
-    var totalBuys : Double = 0.0;
 
     //for every file in the vm list filse open it for reading,
      // itarate through the lines and write to the asm file
@@ -40,42 +41,193 @@ fun iterateFiles(vmFiles: List<File>)
         //print the file name to the asm file
         var fileName = it.toString().substring(it.toString().lastIndexOf("\\") + 1);
         fileName = fileName.substring(0, fileName.lastIndexOf("."));
-        asmFile?.appendText(fileName + "\n");
-
+        asmFile?.appendText("\\"+fileName + "\n");
         inputStream.bufferedReader().useLines { lines -> lines.forEach{
                 //it is a line from the vm file.             
-                  val prices = parseLine(it);
-                  totalSales += prices.first;
-                  totalBuys += prices.second;
+                parseLine(it);
                 } };
-        inputStream.close(); }
-    
-    asmFile?.appendText("TOTAL BUY: $totalBuys\n");
-    asmFile?.appendText("TOTAL CELL: $totalSales\n");   
+        inputStream.close(); } 
 }
-fun parseLine(lineString : String) : Pair<Double,Double>
-{
-    val line = lineString.split(" ");
-    var sellPrice : Double = 0.0;
-    var buyPrice : Double = 0.0;
 
-    //if the line start with cell goto HandleSell, if start with buy goto HandleBuy
+fun parseLine(lineString : String)
+{
+
+    //TODO: if there is any error return the Line number with error message
+    val line = lineString.split(" ");
+
+    asmFile?.appendText("//${line}\n")
     when(line[0])
     {
-        "cell" -> sellPrice+= HandleSell(line[1], line[2].toInt(), line[3].toDouble());
-        "buy" -> buyPrice += HandleBuy(line[1], line[2].toInt(), line[3].toDouble());
+        "add" -> add(line);
+        "sub" -> sub(line);
+        "neg" -> neg(line);
+        "eq" -> eq(line);
+        "gt" -> gt(line);
+        "lt" -> lt(line);
+        "and" -> and(line);
+        "or" -> or(line);
+        "not" -> not(line);
+        "push" -> push(line);
+        "pop" -> pop(line);
     }
-    return Pair(sellPrice, buyPrice);
+    asmFile?.appendText("\n");
 }
-fun HandleBuy(productName: String,amount:Int,price:Double) : Double
+
+
+
+
+fun handleOffsetPush(offset : String, base : Int)
 {
-    asmFile?.appendText("### BUY $productName ###\n");
-    asmFile?.appendText((amount * price).toString() + "\n");    
-    return amount * price;
+    var loc = (Integer.parseInt(offset) + base).toString()
+    asmFile?.appendText("@" + loc + "\n")
+    asmFile?.appendText("D=M\n")
+    asmFile?.appendText("@SP\n")
+    asmFile?.appendText("A=M\n")
+    asmFile?.appendText("M=D\n")
+    asmFile?.appendText("@SP\n")
+    asmFile?.appendText("M=M+1\n")
 }
-fun HandleSell(productName: String,amount:Int,price:Double) : Double
+
+fun handleOffsetPop(offset : String, base : Int)
 {
-    asmFile?.appendText("$$$ CELL $productName $$$\n");
-    asmFile?.appendText((amount * price).toString() + "\n");
-    return amount * price;
+    var loc = (Integer.parseInt(offset) + base).toString()
+    asmFile?.appendText("@" + loc+ "\n")
+    asmFile?.appendText("D=A\n")
+    asmFile?.appendText("@14\n")
+    asmFile?.appendText("M=D\n")
 }
+
+
+fun comment(line : List<String>)
+{
+    asmFile?.appendText("//${line[1]}\n")
+}
+fun push(line : List<String>)
+{
+    if(line[1]=="constant")
+    {
+        asmFile?.appendText("@${line[1]}\n")
+        asmFile?.appendText("D=A\n")
+        asmFile?.appendText("@SP\n")
+        asmFile?.appendText("A=M\n")
+        asmFile?.appendText("M=D\n")
+        asmFile?.appendText("@SP\n")
+        asmFile?.appendText("M=M+1\n")
+    }
+    else if(line[1]=="static")
+    {
+        handleOffsetPush(line[2], 16)
+    }
+    else if(line[1]=="temp")
+    {
+        handleOffsetPush(line[2], 5)
+    }
+    else if(line[1]=="pointer")
+    {
+        handleOffsetPush(line[2], 3)
+
+    }
+    else {
+        //TODO: handle this
+    }
+}
+fun pop(line : List<String>)
+{
+    //take caer of the case where the pop is static, temp and pointer
+    if (line[1] == "static") 
+    {
+        handleOffsetPop(line[2], 16)
+    } else if (line[1] == "temp") 
+    {
+        handleOffsetPop(line[2], 5)
+    } else if (line[1] == "pointer") 
+    {
+        handleOffsetPop(line[2], 3)
+    } 
+    else {}
+    asmFile?.appendText("@SP\n")
+    asmFile?.appendText("AM=M-1\n")
+    asmFile?.appendText("D=M\n")
+    asmFile?.appendText("@14\n")
+    asmFile?.appendText("A=M\n")
+    asmFile?.appendText("M=D\n")
+}
+fun add(line : List<String>)
+{
+    asmFile?.appendText("@SP\n")
+    asmFile?.appendText("AM=M-1\n")
+    asmFile?.appendText("D=M\n")
+    asmFile?.appendText("@SP\n")
+    asmFile?.appendText("AM=M-1\n")
+    asmFile?.appendText("D=D+M\n")
+    asmFile?.appendText("M=D\n")
+    asmFile?.appendText("@SP\n")
+    asmFile?.appendText("M=M+1\n")
+}
+fun sub(line : List<String>)
+{
+    asmFile?.appendText("@SP\n")
+    asmFile?.appendText("AM=M-1\n")
+    asmFile?.appendText("D=M\n")
+    asmFile?.appendText("@SP\n")
+    asmFile?.appendText("AM=M-1\n")
+    asmFile?.appendText("D=D-M\n")
+    asmFile?.appendText("M=D\n")
+    asmFile?.appendText("@SP\n")
+    asmFile?.appendText("M=M+1\n")
+}
+fun neg(line : List<String>)
+{
+    asmFile?.appendText("//--negate--\n")
+    asmFile?.appendText("@SP\n")
+    asmFile?.appendText("AM=M-1\n")
+    asmFile?.appendText("M=-M\n")
+    asmFile?.appendText("@SP\n")
+    asmFile?.appendText("M=M+1\n")
+}
+fun eq(line : List<String>)
+{
+}
+fun gt(line : List<String>)
+{
+}
+fun lt(line : List<String>)
+{
+}
+fun and(line : List<String>)
+{
+    asmFile?.appendText("//--And--\n")
+    asmFile?.appendText("@SP\n")
+    asmFile?.appendText("AM=M-1\n")
+    asmFile?.appendText("D=M\n")
+    asmFile?.appendText("@SP\n")
+    asmFile?.appendText("AM=M-1\n")
+    asmFile?.appendText("D=D&M\n")
+    asmFile?.appendText("M=D\n")
+    asmFile?.appendText("@SP\n")
+    asmFile?.appendText("M=M+1\n")
+}
+fun or(line : List<String>)
+{
+
+    asmFile?.appendText("//--Or--\n")
+    asmFile?.appendText("@SP\n")
+    asmFile?.appendText("AM=M-1\n")
+    asmFile?.appendText("D=M\n")
+    asmFile?.appendText("@SP\n")
+    asmFile?.appendText("AM=M-1\n")
+    asmFile?.appendText("D=D|M\n")
+    asmFile?.appendText("M=D\n")
+    asmFile?.appendText("@SP\n")
+    asmFile?.appendText("M=M+1\n")
+}
+fun not(line : List<String>)
+{
+    asmFile?.appendText("//--Not--\n")
+    asmFile?.appendText("@SP\n")
+    asmFile?.appendText("AM=M-1\n")
+    asmFile?.appendText("M=!M\n")
+    asmFile?.appendText("@SP\n")
+    asmFile?.appendText("M=M+1\n")
+}
+
---
+++ main/kotlin/prsererFunctions.kt
@@ -0,0 +1,3 @@
+package VMcompiler
+
+import kotlin.io.appendText

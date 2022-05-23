c859091554da03a803263c30d3710bb1f54aa9a5
nehorai4775@gmail.com <nehorai4775@gmail.com>
2022-05-23 14:56:55 +0300

    almost finish tar4

--- main/kotlin/Tokenizer.kt
+++ main/kotlin/Tokenizer.kt
@@ -16,115 +16,155 @@ import java.io.PrintWriter
 import java.lang.Character.digit
 
 var xmlFile: File? = null
-val symbols1:List<String> = listOf( "{","}","(",")","[","]",".",",",";","+","-","*","/","&","|","<",">","=","~")
+val symbols1: List<String> =
+    listOf("{", "}", "(", ")", "[", "]", ".", ",", ";", "+", "-", "*", "/", "&", "|", "<", ">", "=", "~")
+val keyword: List<String> = listOf(
+    "class",
+    "constructor",
+    "function",
+    "method",
+    "field",
+    "static",
+    "var",
+    "int",
+    "char",
+    "boolean",
+    "void",
+    "true",
+    "false",
+    "null",
+    "this",
+    "let",
+    "do",
+    "if",
+    "else",
+    "while",
+    "return"
+)
 
 fun main(args: Array<String>) {
-    val jackFiles = setUpFiles2(args[0])
-
-    iterateFiles2(jackFiles)
+    setUpFiles2(args[0])
 
 
 }
 
-fun setUpFiles2(dirName: String): List<File> {
+fun setUpFiles2(dirName: String) {
     //iterate through the directory and find the two .vm files
     val dir = File(dirName)
     val jackFiles = dir.listFiles().filter { it.extension == "jack" }
-    val lastDirName = dirName.substring(dirName.lastIndexOf("\\") + 1)
-    println("lastDirName: $lastDirName")
-    //take the last dir name and create new file in the dir with the name of the dir .xml
-    val xmlFile = File("$dirName\\$lastDirName+T.xml")
-    val FileName = lastDirName
-    xmlFile?.createNewFile()
-    return jackFiles
+    for (file in jackFiles) {
+        var newfileName = file.toString().substring(file.toString().lastIndexOf("\\"))
+        newfileName = newfileName.toString().substring(0, newfileName.toString().lastIndexOf("."))
+        //take the last dir name and create new file in the dir with the name of the dir .xml
+        xmlFile = File("$dirName\\$newfileName"+"T.xml")
+        xmlFile?.createNewFile()
+        iterateFiles2(file)
+    }
 }
 
-fun iterateFiles2(jackFiles: List<File>) {
-    jackFiles.forEach {
-        val inputStream: FileInputStream = File(it.toString()).inputStream()
-        val inputString = inputStream.bufferedReader().use { it.readText() }
+fun iterateFiles2(jackFile: File) {
+    //jackFiles.forEach {
+    val inputStream: FileInputStream = File(jackFile.toString()).inputStream()
+    val inputString = inputStream.bufferedReader().use { jackFile.readText() }
 
-                createXml(inputString)
+    createXml(inputString)
 
-        }
-
-    }
-
-
+}
 
 fun createXml(input: String) {
     val docBuilder: DocumentBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder()
     val doc: Document = docBuilder.newDocument()
     val rootElement: Element = doc.createElement("tokens")
-    var i=0
-    var flag=1
+    var i = 0
+    var flag = 1
 
 
-    var temp:String
-    while(i<input.length){
-        flag=1
+    var temp: String
+    while (i < input.length) {
+        flag = 1
         if (input[i].isDigit()) {
             var temp = input[i].toString()
             i++
-            while (input[i].isDigit()){
+            while (input[i].isDigit()) {
 
-                temp+=input[i].toString()
+                temp += input[i].toString()
                 i++
             }
             val integer: Element = doc.createElement("integerConstant")
             integer.appendChild(doc.createTextNode(temp.toString()))
             rootElement.appendChild(integer)
 
-        }
-        else if(input[i].equals('"')){
+        } else if (input[i].equals('"')) {
             i++
-            temp=input[i].toString()
-            while (!input[i].equals('"')){
+            temp = input[i].toString()
+            while (!input[i].equals('"')) {
 
-                temp+=input[i]
+                temp += input[i]
                 i++
             }
             i++
             val string: Element = doc.createElement("stringConstant")
             string.appendChild(doc.createTextNode(temp.toString()))
             rootElement.appendChild(string)
-        }
-        else if(input[i].equals('/')){
+        } else if (input[i].equals('/')) {
             i++
-            if(input[i].equals('/')){
+            if (input[i].equals('/')) {
                 i++
-                while(!input[i].equals('\n')){
+                while (!input[i].equals('\n')) {
                     i++
                 }
-            }
-            else if(input[i].equals('*')){
+            } else if (input[i].equals('*')) {
                 i++
-                while (flag==1){
+                while (flag == 1) {
                     i++
-                    if(input[i].equals('*')){
+                    if (input[i].equals('*')) {
                         i++
-                        if(input[i].equals('/'))
-                            flag=0
+                        if (input[i].equals('/'))
+                            flag = 0
                     }
                 }
-            }
-            else{
+            } else {
                 val symbol: Element = doc.createElement("symbol")
                 symbol.appendChild(doc.createTextNode("/"))
                 rootElement.appendChild(symbol)
             }
 
-        }
-        else if(symbols1.contains(input[i].toString())) {
-            val symbol: Element = doc.createElement("symbol")
-            symbol.appendChild(doc.createTextNode(input[i].toString()))
-            rootElement.appendChild(symbol)
+        } else if (symbols1.contains(input[i].toString())) {
+            if(input[i].equals('<')){
+                val symbol: Element = doc.createElement("symbol")
+                symbol.appendChild(doc.createTextNode("&gt"))
+                rootElement.appendChild(symbol)
+            }
+            else if(input[i].equals('>')){
+                val symbol: Element = doc.createElement("symbol")
+                symbol.appendChild(doc.createTextNode("&lt"))
+                rootElement.appendChild(symbol)
+            }
+            else {
+                val symbol: Element = doc.createElement("symbol")
+                symbol.appendChild(doc.createTextNode(input[i].toString()))
+                rootElement.appendChild(symbol)
+            }
+        } else if (input[i].isLetter() or input[i].equals("_")) {
+            temp = input[i].toString()
+            i++
+            while (input[i].isLetter() or input[i].equals("_") or input[i].isDigit()) {
+                temp += input[i].toString()
+                i++
+            }
+            if (keyword.contains(temp)) {
+                val keyword1: Element = doc.createElement("keyword")
+                keyword1.appendChild(doc.createTextNode(temp.toString()))
+                rootElement.appendChild(keyword1)
+            } else {
+                val identifier: Element = doc.createElement("identifier")
+                identifier.appendChild(doc.createTextNode(temp.toString()))
+                rootElement.appendChild(identifier)
+            }
         }
         i++
 
     }
-
-
 
 
     // Now, "add" the root node to the XML document in memory
@@ -133,7 +173,7 @@ fun createXml(input: String) {
     val transformer: Transformer = TransformerFactory.newInstance().newTransformer()
 
 
-    transformer.transform(DOMSource(doc), StreamResult("C:\\Devo\\compilers\\src\\main\\kotlin\\kotlin+T.xml"))
+    transformer.transform(DOMSource(doc), StreamResult(xmlFile))
 
 }
 
